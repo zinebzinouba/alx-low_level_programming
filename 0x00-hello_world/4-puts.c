17df3267bc5c6514001ff7b90753347ab47a3973
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-15 03:47:49 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+*main- entry function
+return: 0 if success
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
