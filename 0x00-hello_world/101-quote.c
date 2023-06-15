cce9b91a495f4a23d0a8566292b685538dcd3202
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-15 06:16:46 -0700

    task final

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (not Success)
+ */
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, s, 59);
+	return (1);
+}
