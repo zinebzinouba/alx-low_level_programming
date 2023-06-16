a0f586a315f5b7b2b11dd1cfa344251142b6737a
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-16 12:41:45 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 99)
+	{
+		j = i;
+		while (i <= 99)
+		{
+			if (j != i)
+					{
+						putchar((i / 10) + 48));
+						putchar((i % 10) + 48));
+						putchar(' ');
+						putchar((j / 10) + 48));
+						putchar((j % 10) + 48));
+						if (i != 98 || j != 99)
+						{
+							putchar(',');
+							putchar(' ');
+
+						}
+
+					}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
