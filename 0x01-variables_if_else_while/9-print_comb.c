dcce2597c53267efbf0b2d790e763c4fd6b38a48
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-16 09:53:25 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar ('\n');
+	return (0);
+}
