93f9d732358407f11c48233de344d4ad52f17265
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-16 08:43:09 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if( c != 'e' && c != 'q')
+		{
+		putchar(c);
+		}
+
+	}
+	putchar ('\n');
+	return (0);
+}
