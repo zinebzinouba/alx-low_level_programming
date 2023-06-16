a4bee6eacabf13a995afb1a6fa12224b7bc89198
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-16 12:55:23 -0700

    102 bis

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,20 +18,20 @@ int main(void)
 		while (j <= 99)
 		{
 			if (j != i)
-					{
-						putchar((i / 10) + 48);
-						putchar((i % 10) + 48);
-						putchar(' ');
-						putchar((j / 10) + 48);
-						putchar((j % 10) + 48);
-						if (i != 98 || j != 99)
-						{
-							putchar(',');
-							putchar(' ');
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
 
-						}
+				}
 
-					}
+			}
 			j++;
 		}
 		i++;
