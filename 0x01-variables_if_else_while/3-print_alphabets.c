1c3df947516d12047633df05015be98291979c75
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-16 04:15:05 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+
+	}
+	for (c = 'Z'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar ('\n');
+	return (0);
+}
