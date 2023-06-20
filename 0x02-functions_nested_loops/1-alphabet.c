6efe432443eaaa977674b2b9adffab806b422e87
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-20 10:45:16 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= z; i++)
+	{
+		_putchar(i);
+	}
+return (0);
+}
