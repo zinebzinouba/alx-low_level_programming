3d7e3d1e34f43574064c863a81360b051be883d7
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-20 17:22:10 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ *@a: var
+ *@b: var
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 		_putchar(48);
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar('.');
+			_putchar(',');
 			_putchar(' ');
 			prod = num * mult;
 			if (prod <= 9)
