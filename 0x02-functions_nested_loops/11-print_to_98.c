d1f119d07b0fae159a5604a4d7aaf732de12bc1a
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-20 17:32:34 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ *
+ *@n: check
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+	{
+		for (count = n; count > 98; count--)
+		{
+			printf("%d, ", count);
+		}
+	}
+	else
+	{
+		for (count = n; count < 98; count++)
+		{
+			printf("%d, ", count);
+		}
+	}
+	printf("%98\n");
+}
