aa448d7d12600c274b839691acaf025c4243cd1f
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-21 11:05:12 -0700

    task2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	if (sum > 4000000)
+		break;
+	if ((sum % 2) == 0)
+		total_sum += sum;
+
+	fib1 = fib2;
+	fib2 = sum;
+}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
