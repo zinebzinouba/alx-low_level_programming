8699576e10ba1018b9cb166f328cb746fb863df4
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-22 13:41:32 -0700

    task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - Entry point
+ *
+ *@size: check
+ *
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		_putchar('#');
+		_putchar('\n');
+	}
+
+}
+
