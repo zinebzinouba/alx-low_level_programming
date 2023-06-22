47defe7e95a14349a0d9d49b1e9652a98b37dac0
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-22 16:34:25 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,7 +16,7 @@ void print_triangle(int size)
 	{
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i -1; j++)
+		for (j = 0; j < size - i - 1; j++)
 			_putchar(' ');
 		for (k = 0; k <= i; k++)
 			_putchar('#');
