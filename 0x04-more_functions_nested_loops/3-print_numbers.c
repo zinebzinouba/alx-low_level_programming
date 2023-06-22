bd6a14172b838dc0e94cf57f57efa5e487a5b04e
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-22 09:19:21 -0700

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print__numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
