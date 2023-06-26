ba717a67180f926e1b0eb629133904f86bcc04fe
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-06-26 16:39:47 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - Entry point
+ *
+ * @str: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 ==  0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
