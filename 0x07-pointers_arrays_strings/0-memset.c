b2ca4266807f42b4a54a4eb565f4bf89d079ef95
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-03 16:55:03 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset- Entry point
+ *
+ *@s: check
+ *@b: check
+ *@n:check
+ *
+ * Return: Always 0 (Success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
