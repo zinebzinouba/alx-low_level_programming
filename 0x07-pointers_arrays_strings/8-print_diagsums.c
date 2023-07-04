d666a7c80ebd62c6fa1df3e11e132554a33dbe14
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-03 18:49:39 -0700

    8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagsums- Entry point
+ *
+ *@a: check
+ *@size: check
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[s - i - 1];
+		a += size;
+	}
+	printf("%d, ", s1);
+	printf("%d,\n", s2);
+}
