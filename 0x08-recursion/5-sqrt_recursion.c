0d7d511d5f0ddc8b5ee1c000710d8131068a1fa6
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-05 15:51:01 -0700

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _sqrt_recursion- Entry point
+ *
+ *@n: check
+ *
+ * Return: Always 0 (Success)
+ */
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+/**
+ * square- Entry point
+ *
+ *@n: check
+ *@val: check
+ *
+ * Return: Always 0 (Success)
+ */
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+}
