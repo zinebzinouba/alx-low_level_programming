7c315ba1cb14d8f5b5db463dd465b8cb7ab882cd
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-05 16:07:54 -0700

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ *is_prime_number- Entry point
+ *
+ *@n: check
+ *@o: check
+ *
+ * Return: Always 0 (Success)
+ */
+int premier(int n, int o);
+int is_prime_number(int n)
+{
+	return (premier(n, 2));
+}
+/**
+ * premier- Entry point
+ *
+ *@n: check
+ *@o: check
+ *
+ * Return: Always 0 (Success)
+ */
+int premier(int n, int o)
+{
+	if (o >= n && n > 1)
+		return (1);
+	else if (n % o == 0 || n <= 0)
+		return (0);
+	else
+		return (premier(o, n + 1));
+}
