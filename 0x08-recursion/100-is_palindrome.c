8f881228eed22814b8b567af04707b1d76aecff7
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-05 16:21:18 -0700

    100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *is_palindrome- Entry point
+ *
+ *@s: check
+ *
+ * Return: Always 0 (Success)
+ */
+int is_palindrome(char *s)
+{
+	int NbCaract  = _strlen_recursion(char *s);
+
+	if (NbCaract <= 1)
+		return (1);
+	if (s[0] == s[NbCaract - 1])
+		return (is_palindrome(s + 1, NbCaract - 2));
+	return (0);
+}
