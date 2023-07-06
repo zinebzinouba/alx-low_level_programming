0d11faa09405e6c1c3cdda3692d493d727807093
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-05 17:34:13 -0700

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,33 +1,49 @@
 #include "main.h"
+int is_palindrome(char *s);
+int check(char *s, int start, int end, int mod);
 /**
- *is_palindrome- Entry point
+ * last_index- Entry point
  *
  *@s: check
  *
  * Return: Always 0 (Success)
  */
-int is_palindrome(char *s);
-int check(char *s, int start, int end, int mod);
 int last_index(char *s)
 {
 	int n = 0;
 
 	if (*s != '\0')
-		n += last_index(s +1) + 1;
+		n += last_index(s + 1) + 1;
 	return (n);
 }
+/**
+*is_palindrome- Entry point
+*
+*@s: check
+*
+* Return: Always 0 (Success)
+*/
 int is_palindrome(char *s)
 {
 	int end = last_index(s);
 
 	return (check(s, 0, end - 1, end % 2));
 }
+/**
+* check- Entry point
+*
+*@s: check
+*@start: check
+*@end: check
+*@mod:check
+* Return: Always 0 (Success)
+*/
 int check(char *s, int start, int end, int mod)
 {
-if ((start == end && mod != 0) || (start == end + 1 && mod ==0))
+if ((start == end && mod != 0) || (start == end + 1 && mod == 0))
 	return (1);
 else if (s[start] != s[end])
 	return (0);
 else
-	return (check(s, start +1, end -1, mod));
+	return (check(s, start + 1, end - 1, mod));
 }
