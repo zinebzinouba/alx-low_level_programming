7fb45c2bb676b8510c94d66c54b2d049577398b2
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-05 17:20:18 -0700

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,16 +6,28 @@
  *
  * Return: Always 0 (Success)
  */
+int is_palindrome(char *s);
+int check(char *s, int start, int end, int mod);
+int last_index(char *s)
+{
+	int n = 0;
+
+	if (*s != '\0')
+		n += last_index(s +1) + 1;
+	return (n);
+}
 int is_palindrome(char *s)
 {
-	int NbCaract  = _strlen_recursion(&s);
+	int end = last_index(s);
 
-	if (NbCaract <= 1)
-		return (1);
-	if (s[0] == s[NbCaract - 1])
-	{
-		return (is_palindrome(s + 1));
-				NbCaract -= 2;
-	}
+	return (check(0, end - 1, end % 2);
+}
+int check(char *s, int start, int end, int mod)
+{
+if ((start == end && mos != 0) || (start == end + 1 && mod ==0))
+	return (1);
+else if (s[start] != s[end])
 	return (0);
+else
+	return (check(s, start +1, end -1, mod));
 }
