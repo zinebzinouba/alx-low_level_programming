01b3168f6c97a80a2308f8c0783311f8ac7674c7
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-07 08:15:52 -0700

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *@argc: check
+ *@argv: check
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
