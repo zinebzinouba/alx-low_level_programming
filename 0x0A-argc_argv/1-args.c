58bfc62737c0ee1af54c72da241153d10fcbb6c9
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-07 08:09:16 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *@argc: check
+ *@argv: check
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argv;
+	printf("%s\n", argc - 1);
+	return (0);
+}
