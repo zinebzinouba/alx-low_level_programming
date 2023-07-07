8024e4ea97a9088392e380b2e67527ac86ec85a9
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-07 09:19:15 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ *@argc: check
+ *@argv: check
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	int i, sum;
+	
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < '0' || *argv[i] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
