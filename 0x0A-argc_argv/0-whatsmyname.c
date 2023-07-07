194f1d160e531992f4e79f5ea1ffd8adcd24852b
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-07 06:28:12 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *@argc: check
+ *@argv: check
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
