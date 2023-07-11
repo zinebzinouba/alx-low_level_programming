9513c2b7683d125579feea0a462ab4b0f54dc246
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-11 11:34:32 -0700

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * lenght- Entry point
+ *
+ *@s: check
+ *
+ * Return: Always 0 (Success)
+ */
+int lenght(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
+}
+/**
+ * str_concat- Entry point
+ *
+ *@s1: check
+ *@s2: check
+ * Return: Always 0 (Success)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size1, size2, i;
+	char *m;
+
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
+	size1 = lenght(s1);
+	size2 = lenght(s2);
+	m = malloc((size1 + size2) * sizeof(char) + 1);
+	if (m == 0)
+		return (0);
+	for (i = 0; i <= size1 + size2; i++)
+	{
+		if (i < size1)
+			m[i] = s1[i];
+		else
+			m[i] = s2[i - size1];
+	}
+	m[i] = '\0';
+	return (m);
+}
