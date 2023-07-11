da512cd31a05d1233bd1c08f398c09c60a553ee1
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-11 06:43:16 -0700

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * create_array- Entry point
+ *
+ *@size: check
+ *@c: check
+ *
+ * Return: Always 0 (Success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *n = malloc (size);
+
+	if (size == 0 || n = 0)
+		return (0);
+	while (size--)
+	{
+		n[size] = c;
+	}
+	return (n);
+}
