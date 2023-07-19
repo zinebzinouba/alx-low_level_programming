eeaf50750243243510a786e31beb6b0fbc0350b3
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-07-19 05:28:54 -0700

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,15 +1,17 @@
-#ifndef DOG_H
-#define DOG_H
+#include "dog.h"
 /**
-  *struct dog- so
+  *init_dog- so
+  *@d: dog
   *@name: nom
   *@age: age
   *@owner: propr
   */
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char owner;
-};
-#endif
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
