9ec57e20d3006f65217cfb7f8f92a2a144a4b453
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-08-03 15:59:36 -0700

    h

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,30 +1,22 @@
-#include "holberton.h"
-#include <limits.h>
+#include "main.h"
+
 /**
- * print_binary - prints the binary representation of a number
- * @n: number to convert
- * Return: none
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
-	int i, start;
-
-	start = 0;
-	if (n == 0)
-		_putchar('0');
-	/* how many bits in the machine -1 because index starts at 0*/
-	for (i = sizeof(n) * 8 - 1; i >= 0; i--)
+	if (n >> 0)
 	{
-		/* so it doesn't print all zeros in the beginning */
-		if ((n >> i) & 1)
-			start = 1;
-		/* start printing once you find first 1 */
-		if (start == 1)
-		{
-			if ((n >> i) & 1)
-				_putchar('1');
-			else
-				_putchar('0');
-		}
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
