135eb63c0452cdcbf0d8e9d925b4122d85bdfd7a
zinebzinouba <zineb.najjamchhaibi@gmail.com>
2023-08-03 16:02:12 -0700

    t

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,14 +1,22 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * set_bit - sets the value of a bit to 1 at a given index
- * @n: number
- * @index: index, starting from 0 of the bit you want to set
- * Return: 1 if it worked, or -1 if an error occurred
+ * set_bit - sets the value of a bit to 1.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(n) * 8 - 1)
+	unsigned int m;
+
+	if (index > 63)
 		return (-1);
-	*n = *n | (1 << index);
+
+	m = 1 << index;
+	*n = (*n | m);
+
 	return (1);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,20 +1,22 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * flip_bits - returns the number of bits you would need to flip to get from
- *  one number to another
- * @n: number 1
- * @m: number 2
- * Return: number of bits
+ * flip_bits - returns the number of bits you would
+ * need to flip to get from one number to another
+ * @n: number one.
+ * @m: number two.
+ *
+ * Return: number of bits.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, count;
+	unsigned int nbits;
 
-	count = 0;
-	for (i = sizeof(n) * 8 - 1; i >= 0; i--)
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
 	{
-		if (((n >> i) & 1) ^ ((m >> i) & 1))
-			count++;
+		if ((n & 1) != (m & 1))
+			nbits++;
 	}
-	return (count);
+
+	return (nbits);
 }
